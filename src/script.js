edf8449a8264280a65a901f6ce333f8dbb5007bf
DanKaufmanDev <47069556+DanKaufmanDev@users.noreply.github.com>
2022-12-19 19:51:23 -0500

    Progress Update & Bug Fix

--- src/script.js
+++ src/script.js
@@ -1,29 +1,37 @@
+const zIndexCon = 1
 const terminal = document.querySelector('#terminal')
-const btnMin = document.querySelector('#bar__button--minimize')
-const btnMax = document.querySelector('#bar__button--maximize')
-const appTerminal = document.querySelector('#desktop__background--about').children
+const project = document.querySelector('#project')
 
-// Buttons / Functions
-function buttonClickExit(){
+// Buttons Min/Max?exit
+//Terminal Window
+function buttonClickExitTerminal(){
     terminal.style.display = 'none';
 }
-
-function buttonClickMin(){
+function buttonClickMinTerminal(){
     terminal.style.width = '70vw';
     terminal.style.height = '65vh';
-    btnMax.style.background= 'linear-gradient(#58f358 0%, #12de34 100%)';
 }
-
-function buttonClickMax(){
+function buttonClickMaxTerminal(){
     terminal.style.width = '100vw';
     terminal.style.height = '97.5vh';
-    btnMax.style.background= 'linear-gradient(#7d7871 0%, #595953 100%)';  
 }
 
+//Project Window
+function buttonClickExitProject(){
+    project.style.display = 'none';
+}
+function buttonClickMinProject(){
+    console.log('min clicked')
+}
+function buttonClickMaxProject(){
+    console.log('max clicked')
+    project.style.width = '100vw';
+    project.style.height = '97.5vh';
+}
+
+//Desktop Icons
 function buttonClickAbout(){
-    console.log('clicked terminal')
-    terminal.style.display = 'block',
-    terminal.style.zindex = '-1'
+    terminal.style.display = '';
 }
 
 function buttonClickCalender(){
@@ -39,28 +47,48 @@ function buttonClickMinesweeper(){
 }
 
 function buttonClickProjects(){
-    console.log('clicked projects')
+    project.style.display = 'block';
 }
 
 function buttonClickSpotify(){
     console.log('clicked spotify')
 }
-// Window Movement 
-const wrapper = document.querySelector('#terminal__wrapper'),
-header = wrapper.querySelector('#terminal__bar');
 
-function onDrag({movementX, movementY}){
-    let getStyle = window.getComputedStyle(wrapper);
+// Window Movement 
+//Terminal Window
+const terminalWrapper = document.querySelector('#terminal__wrapper'),
+    terminalHeader = terminalWrapper.querySelector('#terminal__bar');
+
+function onDragTerminal({movementX, movementY}){
+    let getStyle = window.getComputedStyle(terminalWrapper);
     let left = parseInt(getStyle.left);
     let top = parseInt(getStyle.top);
-    wrapper.style.left = `${left + movementX}px`;
-    wrapper.style.top = `${top + movementY}px`;
+    terminalWrapper.style.left = `${left + movementX}px`;
+    terminalWrapper.style.top = `${top + movementY}px`;
 }
-    header.addEventListener("mousedown", () => {
-        header.addEventListener('mousemove', onDrag);
+terminalHeader.addEventListener("mousedown", () => {
+    terminalHeader.addEventListener('mousemove', onDragTerminal);
 });
-    document.addEventListener('mouseup', () =>{
-        header.removeEventListener('mousemove', onDrag);
+document.addEventListener('mouseup', () =>{
+    terminalHeader.removeEventListener('mousemove', onDragTerminal);
 });
 
-// Event Listener
+//Project Window
+const projectWrapper = document.querySelector('#project__wrapper'),
+projectHeader = projectWrapper.querySelector('#project__bar');
+
+function onDragProject({movementX, movementY}){
+    let getStyle = window.getComputedStyle(projectWrapper);
+    let left = parseInt(getStyle.left);
+    let top = parseInt(getStyle.top);
+    projectWrapper.style.left = `${left + movementX}px`;
+    projectWrapper.style.top = `${top + movementY}px`;
+}
+projectHeader.addEventListener("mousedown", () => {
+    projectHeader.addEventListener('mousemove', onDragProject);
+});
+document.addEventListener('mouseup', () =>{
+    projectHeader.removeEventListener('mousemove', onDragProject);
+});
+
+//Minesweeper Window
