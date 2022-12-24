5358bf3fd0fc6b3461b449054b3fcd96aef50273
DanKaufmanDev <47069556+DanKaufmanDev@users.noreply.github.com>
2022-12-23 20:50:22 -0500

    Feature Update & Theme Update

--- src/script.js
+++ src/script.js
@@ -2,8 +2,9 @@ const terminal = document.querySelector('#terminal')
 const minesweeper = document.querySelector('#minesweeper')
 const project = document.querySelector('#project')
 const contact = document.querySelector('#contact')
+const settings = document.querySelector('#desktop__settings')
 
-// Buttons Min/Max?exit
+// Buttons Min/Max/exit
 //Terminal Window
 function buttonClickExitTerminal(){
     terminal.style.display = 'none';
@@ -12,7 +13,6 @@ function buttonClickMinTerminal(){
 }
 function buttonClickMaxTerminal(){
 }
-
 //Project Window
 function buttonClickExitProject(){
     project.style.display = 'none';
@@ -21,7 +21,6 @@ function buttonClickMinProject(){
 }
 function buttonClickMaxProject(){
 }
-
 //Minesweeper Window
 function buttonClickExitMinesweeper(){
     minesweeper.style.display = 'none';
@@ -30,7 +29,6 @@ function buttonClickMinMinesweeper(){
 }
 function buttonClickMaxMinesweeper(){
 }
-
 //Contact Window
 function buttonClickExitContact(){
     contact.style.display = 'none';
@@ -41,22 +39,69 @@ function buttonClickMaxContact(){
 }
 
 //Desktop Icons
+//Desktop About Icon
 function buttonClickAbout(){
     terminal.style.display = 'block';
 }
-
+//Desktop Contact Icon
 function buttonClickContacts(){
     contact.style.display = 'block';
 }
-
+//Desktop Minesweeper Icon
 function buttonClickMinesweeper(){
     minesweeper.style.display = 'block';
 }
-
+//Desktop Project Icon
 function buttonClickProjects(){
     project.style.display = 'block';
 }
 
+//Taskbar
+//Taskbar Settings
+let bool = false;
+function buttonClickSettings(){
+    if (bool === false) {
+    bool = true;
+    settings.style.transform = 'translateX(75vw)';
+    document.querySelector('#terminal__wrapper').style.zIndex = '-1';
+    document.querySelector('#contact__wrapper').style.zIndex = '-1'
+    document.querySelector('#minesweeper__wrapper').style.zIndex = '-1';
+    document.querySelector('#project__wrapper').style.zIndex = '-1';
+    } else {
+    bool = false;
+    settings.style.transform = 'translateX(100vw)';
+    document.querySelector('#terminal__wrapper').style.zIndex = 'auto';
+    document.querySelector('#contact__wrapper').style.zIndex = 'auto';
+    document.querySelector('#minesweeper__wrapper').style.zIndex = 'auto';
+    document.querySelector('#project__wrapper').style.zIndex = 'auto';
+    } 
+}
+
+//Themes
+//Triangles
+function buttonClickTriangle(){
+    document.body.style.background = 'url(background/triangles.svg)';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+//Swirl
+function buttonClickSwirl(){
+    document.body.style.background = 'url(background/swirl.jpg)';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+//Cube
+function buttonClickCube(){
+    document.body.style.background = 'url(background/cube.jpg)';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+//wave
+function buttonClickWave(){
+    document.body.style.background = 'url(background/wave.jpg)';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
 
 // Window Movement 
 //Terminal Window
@@ -76,7 +121,6 @@ terminalHeader.addEventListener("mousedown", () => {
 document.addEventListener('mouseup', () =>{
     terminalHeader.removeEventListener('mousemove', onDragTerminal);
 });
-
 //Project Window
 const projectWrapper = document.querySelector('#project__wrapper'),
 projectHeader = projectWrapper.querySelector('#project__bar');
@@ -94,7 +138,6 @@ projectHeader.addEventListener("mousedown", () => {
 document.addEventListener('mouseup', () =>{
     projectHeader.removeEventListener('mousemove', onDragProject);
 });
-
 //Minesweeper Window
 const minesweeperWrapper = document.querySelector('#minesweeper__wrapper'),
     minesweeperHeader = minesweeperWrapper.querySelector('#minesweeper__bar');
@@ -112,7 +155,6 @@ minesweeperHeader.addEventListener("mousedown", () => {
 document.addEventListener('mouseup', () => {
     minesweeperHeader.removeEventListener('mousemove', onDragMinesweeper);
 });
-
 //Contact Window
 const contactWrapper = document.querySelector('#contact__wrapper'),
     contactHeader = contactWrapper.querySelector('#contact__bar');
